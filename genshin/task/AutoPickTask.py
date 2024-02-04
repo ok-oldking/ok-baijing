c94ede241df1112a2cc225c398e2762c87c8fa04
cto <154002444+CrashTechnologyOfficer@users.noreply.github.com>
2024-02-04 11:50:08 +0800

    add requirements

--- genshin/task/AutoPickTask.py
+++ genshin/task/AutoPickTask.py
@@ -16,7 +16,7 @@ class AutoPickTask(FindFeatureTask):
             button_f = self.find_one(frame, "button_f", 0.3, 0.3)
             if button_f:
                 if not self.has_dialogs(frame):
-                    time.sleep(0.2)
+                    time.sleep(0.1)
                     next_frame = executor.next_frame()
                     if self.find_one(next_frame, "button_f", 0.3, 0.3) and not self.has_dialogs(next_frame):
                         print("AutoPickTask: double check passed click f")
@@ -24,33 +24,13 @@ class AutoPickTask(FindFeatureTask):
                         time.sleep(0.1)
                         self.interaction.send_key("f")
                         time.sleep(0.1)
-
-            # if button_f:  # if there is an f, we want to find only one choice that is not a Dialog
-            #     to_find = Box(button_f.x + int(button_f.width * 2.2), button_f.y, button_f.width, button_f.height)
-            #     choice = find_choice(frame, to_find, threshold=0.4)
-            #     if choice:
-            #         self.draw_box("choices", choice)
-            #         if True or self.find(frame, "button_dialog", 0.5, 0.5):
-            #             return
-            #         else:
-            #             time.sleep(0.01)
-            #             # frame = executor.next_frame()
-            #             # if not self.find(frame, "button_dialog", 0.7, 0.7):
-            #             #     print(f"found button_f with no dialog pickup")
-            #             print(f"found button_f with no dialog pickup")
-            #             self.interaction.send_key("f")
-            #             time.sleep(0.1)
-            #             self.interaction.send_key("f")
-            #             time.sleep(0.1)
-            #             # self.interaction.send_key("f")
-            #             # time.sleep(0.1)
-            #             # self.interaction.send_key("f")
-            #             # time.sleep(0.1)
-            #             # self.interaction.send_key("f")
-            #             # time.sleep(0.1)
+                        self.interaction.send_key("f")
+                        time.sleep(0.1)
+                        self.interaction.send_key("f")
+                        time.sleep(0.1)
 
     def has_dialogs(self, frame: MatLike):
-        dialogs = self.find(frame, "button_dialog", 0.4, 0.4)
+        dialogs = self.find(frame, "button_dialog_world", 0.4, 0.4)
         if len(dialogs) > 0:
             return True
         else:
