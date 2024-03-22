75ef699def3e8736c49669bb62dbd2858fcf7dcd
cto <firedcto@gmail.com>
2024-03-22 21:16:48 +0800

    fix scaling on hight resolution

--- autohelper/capture/windows/window.py
+++ autohelper/capture/windows/window.py
@@ -28,15 +28,16 @@ def get_window_bounds(hwnd):
     # print(f"get_window_bounds: scaling:{scaling},window_rect:{window_rect}, client:({client_x},{client_y},{client_width},{client_height}))")
     # window_left_bounds = extended_frame_bounds.left - window_rect[0]
     # window_top_bounds = extended_frame_bounds.top - window_rect[1]
-    client_width = int(client_width * scaling)
-    client_height = int(client_height * scaling)
-    window_width = extended_frame_bounds.right - extended_frame_bounds.left
-    window_height = extended_frame_bounds.bottom - extended_frame_bounds.top
+    client_width = int(client_width / scaling)
+    client_height = int(client_height / scaling)
+    window_width = int((extended_frame_bounds.right - extended_frame_bounds.left) / scaling)
+    window_height = int((extended_frame_bounds.bottom - extended_frame_bounds.top) / scaling)
     # print(f"window_width:{window_width} client_width:{client_width}")
     border = int((window_width - client_width) / 2)
     title = window_height - client_height - border
     # print(f"{border}, {title}, {client_width}, {client_height}")
-    return extended_frame_bounds.left, extended_frame_bounds.top, border, title, client_width, client_height, scaling
+    return int(extended_frame_bounds.left / scaling), int(
+        extended_frame_bounds.top / scaling), border, title, client_width, client_height, scaling
 
 
 def is_foreground_window(hwnd):
