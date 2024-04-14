30b594bc3d535a10d5ae91596c094f122f37a288
cto <firedcto@gmail.com>
2024-04-14 22:21:41 +0800

    add about

--- config.py
+++ config.py
@@ -8,12 +8,17 @@ config = {
     'config_folder': 'configs',
     'gui_icon': get_path_in_package(__file__, 'icon.ico'),
     'ocr': 'RapidOCR',
+    'about': """
+    <h1>基于ok-script的白荆回廊自动漫巡辅助</h1>
+    <p>Github下载和源码地址:</p>
+    <p><a href="https://github.com/ok-oldking/ok_baijing">https://github.com/ok-oldking/ok_baijing</a></p>
+""",
     'gui_title': 'OK白荆漫巡',  # Optional, default: False
     'capture': 'adb',  # adb/windows, see #ok.capture
     'capture_window_title': r'^白荆回廊\[[0-9.]+\]$',  # required  when using windows capture
     # 'coco_feature_folder': get_path(__file__, 'assets/coco_feature'),  # required if using feature detection
-    'log_file': 'logs/auto_helper.log',  # Optional, auto rotating every day
-    'error_log_file': 'logs/auto_helper_error.log',
+    'log_file': 'logs/ok-script.log',  # Optional, auto rotating every day
+    'error_log_file': 'logs/ok-script_error.log',
     'tasks': [  # tasks to execute
         ManXunTask(),
     ], 'scenes': [  # scenes to detect
