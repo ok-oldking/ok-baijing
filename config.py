765a99fc2aff0d2bc932e21d42b26cacc55ad56d
okpc\ok <firedcto@gmail.com>
2024-05-29 14:33:59 +0800

    1.检查支持分辨率 2.检查是否中文目录（openvino发布版暂不支持读取中文目录模型） 3.PC版处于后台添加暂停提示

--- config.py
+++ config.py
@@ -17,9 +17,10 @@ config = {
     <h1>OK白荆回廊自动漫巡辅助</h1>
     <p>QQ群:594495691</p>
 """,
+    'supported_screen_ratio': '16:9',
     'screenshots_folder': "screenshots",
     'gui_title': 'OK白荆漫巡',  # Optional
-    'capture_window_title': r'^白荆回廊\[[0-9.]+\]$',  # required  when using windows capture
+    'capture_window_title': r'^白荆回廊',  # required  when using windows capture
     'capture_window_exe_name': 'GateMK-Win64-Shipping.exe',
     # 'coco_feature_folder': get_path(__file__, 'assets/coco_feature'),  # required if using feature detection
     'log_file': 'logs/ok-script.log',  # Optional, auto rotating every day
